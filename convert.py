ce289c31215932e4b8d332a35400dbecbc0b12e9
irodori038 <funanori9292@gmail.com>
2017-08-30 13:11:33 +0900

    deg2cartesian finished

--- convert.py
+++ convert.py
@@ -1,11 +1,14 @@
-from math import sqrt, sin, cos
+from math import sqrt, sin, cos, pi
 
 
-def deg2cartesian(phi, lamda, h):
+def deg2cartesian(lat, lon, h):
     """convert lot & lag to earth centered xyz position"""
     f = 1 / 298.257223563
-    e = sqrt(f * (2 - f))
-    N = 6378.137 / (2 * (1 - e ** 2 * (sin(phi) ** 2)))
+    a = 6378137
+    phi = lat * pi / 180
+    lamda = lon * pi / 180
+    e = sqrt(2 * f - f * f)
+    N = a / sqrt(1 - (e * sin(phi)) ** 2)
 
     x = (N + h) * cos(phi) * cos(lamda)
     y = (N + h) * cos(phi) * sin(lamda)
