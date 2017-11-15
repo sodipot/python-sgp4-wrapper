Fixed azimuth calculation

convert.py
@@ -118,8 +118,11 @@ def sat2direction(observer, satposition):
 
     rdirection = conv3 * conv2 * rvec
 
-    azimuth = np.arctan(-rdirection[1, 0]/rdirection[0, 0])
-    azimuth = np.rad2deg(azimuth) + 180.0
+    azimuth = np.rad2deg(np.arctan2(-rdirection[1, 0], rdirection[0, 0]))
+    if rdirection[0, 0] < 0:
+        azimuth += 180.0
+    if azimuth < 0:
+        azimuth += 360.0
     if azimuth >= 360.0:
         azimuth -= 360.0
 